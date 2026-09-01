71c74acb03449dc21cf7ea41f6a87fa9a0269db1
kelly677 <167341002+kelly677@users.noreply.github.com>
2026-09-01 12:26:24 +0200

    Created folder structure (Classes, Forms, File) and added Tenant class + TenantsForm + File Class

---
+++ RentFlow_Application/RentFlow_Application/File Data/File Manager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentFlow_Application.File_Data
+{
+    public class File_Manager
+    {
+        // TO DO: Member 7 will implement JSON saving/loading here
+    }
+}
---
+++ RentFlow_Application/RentFlow_Application/Forms/Tenants ManagementForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RentFlow_Application.Forms
+{
+    public partial class Tenants_ManagementForm : Form
+    {
+        public Tenants_ManagementForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
