ee0374e868b022464274184315d2ab962c878681
Owam <owamlukhele06@gmail.com>
2026-09-04 16:37:45 +0200

    Created Create New Lease form design

---
+++ RentFlow_Application/RentFlow_Application/Forms/CreateLeaseForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RentFlow_Application.Forms
+{
+    public partial class CreateLeaseForm : Form
+    {
+        public CreateLeaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateLeaseForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
