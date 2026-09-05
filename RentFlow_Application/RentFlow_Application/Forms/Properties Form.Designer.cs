9605c8481c38a319ee0e5eb6ffb870c1bd2b3a7f
Sisanda520! <thandosisanda5@gmail.com>
2026-09-05 19:41:22 +0200

    # I have done the properties class and design

---
+++ RentFlow_Application/RentFlow_Application/Forms/Properties Form.Designer.cs
@@ -0,0 +1,423 @@
+﻿namespace RentFlow_Application.Forms
+{
+    partial class Properties_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlSideBar = new Panel();
+            btnMaintenance = new Button();
+            btnExpenses = new Button();
+            btnRentPayments = new Button();
+            btnLeases = new Button();
+            btnTenants = new Button();
+            btnRentalUnits = new Button();
+            btnProperties = new Button();
+            btnDashboard = new Button();
+            lblLogo = new Label();
+            lblLogoIcon = new Label();
+            pnlMain = new Panel();
+            dgvProperties = new DataGridView();
+            PropertyID = new DataGridViewTextBoxColumn();
+            PropertyName = new DataGridViewTextBoxColumn();
+            Address = new DataGridViewTextBoxColumn();
+            TotalUnits = new DataGridViewTextBoxColumn();
+            Occupied = new DataGridViewTextBoxColumn();
+            Available = new DataGridViewTextBoxColumn();
+            Status = new DataGridViewTextBoxColumn();
+            Actions = new DataGridViewTextBoxColumn();
+            pnlSearch = new Panel();
+            btnPrint = new Button();
+            button1 = new Button();
+            cmbFilter = new ComboBox();
+            txtSearch = new TextBox();
+            btnAddProperty = new Button();
+            lblPropertyCount = new Label();
+            lblProperties = new Label();
+            pnlSideBar.SuspendLayout();
+            pnlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvProperties).BeginInit();
+            pnlSearch.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlSideBar
+            // 
+            pnlSideBar.BackColor = Color.DarkBlue;
+            pnlSideBar.Controls.Add(btnMaintenance);
+            pnlSideBar.Controls.Add(btnExpenses);
+            pnlSideBar.Controls.Add(btnRentPayments);
+            pnlSideBar.Controls.Add(btnLeases);
+            pnlSideBar.Controls.Add(btnTenants);
+            pnlSideBar.Controls.Add(btnRentalUnits);
+            pnlSideBar.Controls.Add(btnProperties);
+            pnlSideBar.Controls.Add(btnDashboard);
+            pnlSideBar.Controls.Add(lblLogo);
+            pnlSideBar.Controls.Add(lblLogoIcon);
+            pnlSideBar.Location = new Point(12, 12);
+            pnlSideBar.Name = "pnlSideBar";
+            pnlSideBar.Size = new Size(231, 735);
+            pnlSideBar.TabIndex = 0;
+            // 
+            // btnMaintenance
+            // 
+            btnMaintenance.FlatStyle = FlatStyle.Flat;
+            btnMaintenance.ForeColor = Color.White;
+            btnMaintenance.Location = new Point(10, 422);
+            btnMaintenance.Name = "btnMaintenance";
+            btnMaintenance.Size = new Size(210, 40);
+            btnMaintenance.TabIndex = 9;
+            btnMaintenance.Text = "Maintenance";
+            btnMaintenance.UseVisualStyleBackColor = true;
+            // 
+            // btnExpenses
+            // 
+            btnExpenses.FlatStyle = FlatStyle.Flat;
+            btnExpenses.ForeColor = Color.White;
+            btnExpenses.Location = new Point(10, 376);
+            btnExpenses.Name = "btnExpenses";
+            btnExpenses.Size = new Size(210, 40);
+            btnExpenses.TabIndex = 8;
+            btnExpenses.Text = "Expenses";
+            btnExpenses.UseVisualStyleBackColor = true;
+            // 
+            // btnRentPayments
+            // 
+            btnRentPayments.FlatStyle = FlatStyle.Flat;
+            btnRentPayments.ForeColor = Color.White;
+            btnRentPayments.Location = new Point(10, 330);
+            btnRentPayments.Name = "btnRentPayments";
+            btnRentPayments.Size = new Size(210, 40);
+            btnRentPayments.TabIndex = 7;
+            btnRentPayments.Text = "Rent Payments";
+            btnRentPayments.UseVisualStyleBackColor = true;
+            // 
+            // btnLeases
+            // 
+            btnLeases.FlatStyle = FlatStyle.Flat;
+            btnLeases.ForeColor = Color.White;
+            btnLeases.Location = new Point(10, 284);
+            btnLeases.Name = "btnLeases";
+            btnLeases.Size = new Size(210, 40);
+            btnLeases.TabIndex = 6;
+            btnLeases.Text = "Leases";
+            btnLeases.UseVisualStyleBackColor = true;
+            // 
+            // btnTenants
+            // 
+            btnTenants.FlatStyle = FlatStyle.Flat;
+            btnTenants.ForeColor = Color.White;
+            btnTenants.Location = new Point(10, 238);
+            btnTenants.Name = "btnTenants";
+            btnTenants.Size = new Size(210, 40);
+            btnTenants.TabIndex = 5;
+            btnTenants.Text = " Tenants";
+            btnTenants.UseVisualStyleBackColor = true;
+            // 
+            // btnRentalUnits
+            // 
+            btnRentalUnits.FlatStyle = FlatStyle.Flat;
+            btnRentalUnits.ForeColor = Color.White;
+            btnRentalUnits.Location = new Point(10, 192);
+            btnRentalUnits.Name = "btnRentalUnits";
+            btnRentalUnits.Size = new Size(210, 40);
+            btnRentalUnits.TabIndex = 4;
+            btnRentalUnits.Text = "Rental Units";
+            btnRentalUnits.UseVisualStyleBackColor = true;
+            // 
+            // btnProperties
+            // 
+            btnProperties.BackColor = Color.LightSteelBlue;
+            btnProperties.FlatStyle = FlatStyle.Flat;
+            btnProperties.ForeColor = Color.Black;
+            btnProperties.Location = new Point(10, 146);
+            btnProperties.Name = "btnProperties";
+            btnProperties.Size = new Size(210, 40);
+            btnProperties.TabIndex = 3;
+            btnProperties.Text = "Properties";
+            btnProperties.UseVisualStyleBackColor = false;
+            // 
+            // btnDashboard
+            // 
+            btnDashboard.FlatStyle = FlatStyle.Flat;
+            btnDashboard.ForeColor = Color.White;
+            btnDashboard.Location = new Point(10, 100);
+            btnDashboard.Name = "btnDashboard";
+            btnDashboard.Size = new Size(210, 40);
+            btnDashboard.TabIndex = 2;
+            btnDashboard.Text = "Dashboard";
+            btnDashboard.UseVisualStyleBackColor = true;
+            // 
+            // lblLogo
+            // 
+            lblLogo.AutoSize = true;
+            lblLogo.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblLogo.ForeColor = Color.White;
+            lblLogo.Location = new Point(59, 24);
+            lblLogo.Name = "lblLogo";
+            lblLogo.Size = new Size(145, 41);
+            lblLogo.TabIndex = 1;
+            lblLogo.Text = "RentFlow";
+            // 
+            // lblLogoIcon
+            // 
+            lblLogoIcon.AutoSize = true;
+            lblLogoIcon.Font = new Font("Segoe UI Semibold", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblLogoIcon.ForeColor = Color.White;
+            lblLogoIcon.Location = new Point(15, 27);
+            lblLogoIcon.Name = "lblLogoIcon";
+            lblLogoIcon.Size = new Size(48, 38);
+            lblLogoIcon.TabIndex = 0;
+            lblLogoIcon.Text = "RF";
+            // 
+            // pnlMain
+            // 
+            pnlMain.BackColor = Color.White;
+            pnlMain.BorderStyle = BorderStyle.Fixed3D;
+            pnlMain.Controls.Add(dgvProperties);
+            pnlMain.Controls.Add(pnlSearch);
+            pnlMain.Controls.Add(btnAddProperty);
+            pnlMain.Controls.Add(lblPropertyCount);
+            pnlMain.Controls.Add(lblProperties);
+            pnlMain.Location = new Point(250, 8);
+            pnlMain.Name = "pnlMain";
+            pnlMain.Size = new Size(1119, 729);
+            pnlMain.TabIndex = 1;
+            pnlMain.Paint += pnlMain_Paint;
+            // 
+            // dgvProperties
+            // 
+            dgvProperties.AllowUserToAddRows = false;
+            dgvProperties.AllowUserToDeleteRows = false;
+            dgvProperties.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvProperties.BackgroundColor = Color.White;
+            dgvProperties.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvProperties.Columns.AddRange(new DataGridViewColumn[] { PropertyID, PropertyName, Address, TotalUnits, Occupied, Available, Status, Actions });
+            dgvProperties.Location = new Point(30, 310);
+            dgvProperties.MultiSelect = false;
+            dgvProperties.Name = "dgvProperties";
+            dgvProperties.ReadOnly = true;
+            dgvProperties.RowHeadersWidth = 51;
+            dgvProperties.Size = new Size(1055, 410);
+            dgvProperties.TabIndex = 4;
+            // 
+            // PropertyID
+            // 
+            PropertyID.HeaderText = "PropertyID";
+            PropertyID.MinimumWidth = 6;
+            PropertyID.Name = "PropertyID";
+            PropertyID.ReadOnly = true;
+            // 
+            // PropertyName
+            // 
+            PropertyName.HeaderText = "PropertyName";
+            PropertyName.MinimumWidth = 6;
+            PropertyName.Name = "PropertyName";
+            PropertyName.ReadOnly = true;
+            // 
+            // Address
+            // 
+            Address.HeaderText = "Address";
+            Address.MinimumWidth = 6;
+            Address.Name = "Address";
+            Address.ReadOnly = true;
+            // 
+            // TotalUnits
+            // 
+            TotalUnits.HeaderText = "TotalUnits";
+            TotalUnits.MinimumWidth = 6;
+            TotalUnits.Name = "TotalUnits";
+            TotalUnits.ReadOnly = true;
+            // 
+            // Occupied
+            // 
+            Occupied.HeaderText = "Occupied";
+            Occupied.MinimumWidth = 6;
+            Occupied.Name = "Occupied";
+            Occupied.ReadOnly = true;
+            // 
+            // Available
+            // 
+            Available.HeaderText = "Available";
+            Available.MinimumWidth = 6;
+            Available.Name = "Available";
+            Available.ReadOnly = true;
+            // 
+            // Status
+            // 
+            Status.HeaderText = "Status";
+            Status.MinimumWidth = 6;
+            Status.Name = "Status";
+            Status.ReadOnly = true;
+            // 
+            // Actions
+            // 
+            Actions.HeaderText = "Actions";
+            Actions.MinimumWidth = 6;
+            Actions.Name = "Actions";
+            Actions.ReadOnly = true;
+            // 
+            // pnlSearch
+            // 
+            pnlSearch.BackColor = Color.Gainsboro;
+            pnlSearch.BorderStyle = BorderStyle.Fixed3D;
+            pnlSearch.Controls.Add(btnPrint);
+            pnlSearch.Controls.Add(button1);
+            pnlSearch.Controls.Add(cmbFilter);
+            pnlSearch.Controls.Add(txtSearch);
+            pnlSearch.Location = new Point(30, 195);
+            pnlSearch.Name = "pnlSearch";
+            pnlSearch.Size = new Size(1055, 50);
+            pnlSearch.TabIndex = 3;
+            // 
+            // btnPrint
+            // 
+            btnPrint.FlatStyle = FlatStyle.System;
+            btnPrint.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnPrint.Location = new Point(954, 10);
+            btnPrint.Name = "btnPrint";
+            btnPrint.Size = new Size(94, 29);
+            btnPrint.TabIndex = 3;
+            btnPrint.Text = "Print";
+            btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.System;
+            button1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(857, 11);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Export";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // cmbFilter
+            // 
+            cmbFilter.FormattingEnabled = true;
+            cmbFilter.Items.AddRange(new object[] { "All", "", "Active", "", "Inactive" });
+            cmbFilter.Location = new Point(376, 11);
+            cmbFilter.Name = "cmbFilter";
+            cmbFilter.Size = new Size(110, 28);
+            cmbFilter.TabIndex = 1;
+            cmbFilter.SelectedIndexChanged += cmbFilter_SelectedIndexChanged;
+            // 
+            // txtSearch
+            // 
+            txtSearch.ForeColor = Color.LightGray;
+            txtSearch.Location = new Point(23, 12);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(335, 27);
+            txtSearch.TabIndex = 0;
+            txtSearch.Text = "Search properties";
+            txtSearch.TextChanged += txtSearch_TextChanged;
+            // 
+            // btnAddProperty
+            // 
+            btnAddProperty.BackColor = Color.DarkBlue;
+            btnAddProperty.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAddProperty.ForeColor = Color.White;
+            btnAddProperty.Location = new Point(920, 120);
+            btnAddProperty.Name = "btnAddProperty";
+            btnAddProperty.Size = new Size(165, 45);
+            btnAddProperty.TabIndex = 2;
+            btnAddProperty.Text = " +Add Property";
+            btnAddProperty.UseVisualStyleBackColor = false;
+            btnAddProperty.Click += btnAddProperty_Click;
+            // 
+            // lblPropertyCount
+            // 
+            lblPropertyCount.AutoSize = true;
+            lblPropertyCount.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPropertyCount.ForeColor = Color.FromArgb(64, 64, 64);
+            lblPropertyCount.Location = new Point(18, 77);
+            lblPropertyCount.Name = "lblPropertyCount";
+            lblPropertyCount.Size = new Size(194, 28);
+            lblPropertyCount.TabIndex = 1;
+            lblPropertyCount.Text = "4 Properties in total";
+            // 
+            // lblProperties
+            // 
+            lblProperties.AutoSize = true;
+            lblProperties.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblProperties.Location = new Point(18, 36);
+            lblProperties.Name = "lblProperties";
+            lblProperties.Size = new Size(164, 41);
+            lblProperties.TabIndex = 0;
+            lblProperties.Text = "Properties";
+            // 
+            // Properties_Form
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1379, 732);
+            Controls.Add(pnlMain);
+            Controls.Add(pnlSideBar);
+            Name = "Properties_Form";
+            Text = "Properties_Form";
+            Load += Properties_Form_Load;
+            pnlSideBar.ResumeLayout(false);
+            pnlSideBar.PerformLayout();
+            pnlMain.ResumeLayout(false);
+            pnlMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvProperties).EndInit();
+            pnlSearch.ResumeLayout(false);
+            pnlSearch.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlSideBar;
+        private Button btnDashboard;
+        private Label lblLogo;
+        private Label lblLogoIcon;
+        private Button btnMaintenance;
+        private Button btnExpenses;
+        private Button btnRentPayments;
+        private Button btnLeases;
+        private Button btnTenants;
+        private Button btnRentalUnits;
+        private Button btnProperties;
+        private Panel pnlMain;
+        private Label lblProperties;
+        private Label lblPropertyCount;
+        private Button btnAddProperty;
+        private Panel pnlSearch;
+        private TextBox txtSearch;
+        private ComboBox cmbFilter;
+        private Button button1;
+        private Button btnPrint;
+        private DataGridView dgvProperties;
+        private DataGridViewTextBoxColumn PropertyID;
+        private DataGridViewTextBoxColumn PropertyName;
+        private DataGridViewTextBoxColumn Address;
+        private DataGridViewTextBoxColumn TotalUnits;
+        private DataGridViewTextBoxColumn Occupied;
+        private DataGridViewTextBoxColumn Available;
+        private DataGridViewTextBoxColumn Status;
+        private DataGridViewTextBoxColumn Actions;
+    }
+}
